ed22ff0a733ecf977597c2e27c671f365eb905d3
1103git <103031812+1103git@users.noreply.github.com>
2022-04-14 18:03:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 ### import the packages
-import flask
 from flask import Flask, jsonify, request
 # import requests, json
 # instantiate the class
@@ -10,18 +9,10 @@ def gather():
     data = request.get_json() # fetch the payload and 
     output = None
     height = data['height']         # desearlize it and capture the data into identifiers
-    print(height)
     weight = data['weight']
-    print(weight)
-    bmi=int(weight) / int(height/100)**2  
-    print(bmi)
-    return(jsonify({"output": output}))
-
-@app.route('/details', methods=['GET'])
-def fetch():
-
-    details = [1,2,43]
-    return(jsonify({"output":details}))
+    output = weight/pow(height,2)
+    return(jsonify({"BMI": output}))
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
+    app.run(debug=True)
